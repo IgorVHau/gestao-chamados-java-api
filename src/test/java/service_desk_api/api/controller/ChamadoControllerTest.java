0d9466f500be4d40928928bc0152ad513254b723
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-12-17 22:10:38 -0300

    🧪 test: Adicionar teste de controller para retorno 404 em recurso inexistente - 17/12/2025

---
+++ src/test/java/service_desk_api/api/controller/ChamadoControllerTest.java
@@ -0,0 +1,53 @@
+package service_desk_api.api.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.web.servlet.MockMvc;
+
+import service_desk_api.api.dto.JwtUtil;
+import service_desk_api.api.exception.ResourceNotFoundException;
+import service_desk_api.api.service.ChamadoService;
+
+@WebMvcTest(
+		controllers = ChamadoController.class,
+		excludeAutoConfiguration = SecurityAutoConfiguration.class
+		)
+class ChamadoControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ChamadoService chamadoService;
+	
+	@MockBean
+	private JwtUtil jwtUtil;
+	
+	@MockBean
+	private UserDetailsService userDetailsService;
+	
+	@Test
+	void deveRetornar404QuandoBuscarPorIdInexistente() throws Exception{
+		
+		when(chamadoService.buscarPorId(99L)).thenThrow(new ResourceNotFoundException("Chamado não encontrado."));
+		
+		mockMvc.perform(get("/chamados/99"))
+			.andExpect(status().isNotFound())
+			//.andExpect(jsonPath("$.timestamp").value(LocalDateTime.now()))
+			.andExpect(jsonPath("$.status").value(404))
+			.andExpect(jsonPath("$.message").value("Chamado não encontrado."))
+			.andExpect(jsonPath("$.data").isEmpty());
+	}
+
+}
