d255b0da0dfc3e183854869440fe992f912c7e64
IgorVHau <89521633+IgorVHau@users.noreply.github.com>
2025-12-20 17:10:49 -0300

    🔄️ feat: Validar atualização de chamados concluídos - 20/12/2025

--- src/main/java/service_desk_api/api/service/ChamadoService.java
+++ src/main/java/service_desk_api/api/service/ChamadoService.java
@@ -1,6 +1,9 @@
 package service_desk_api.api.service;
 
 import org.springframework.stereotype.Service;
+
+import service_desk_api.api.exception.BusinessException;
+import service_desk_api.api.exception.ResourceNotFoundException;
 import service_desk_api.api.model.Chamado;
 import service_desk_api.api.model.Status;
 import service_desk_api.api.repository.ChamadoRepository;
@@ -31,15 +34,34 @@ public class ChamadoService {
 		return repository.save(chamado);
 	}
 	
+//	public Chamado atualizar(Long id, Chamado novoChamado) {
+//		return repository.findById(id)
+//				.map(c -> {
+//					c.setTitulo(novoChamado.getTitulo());
+//					c.setDescricao(novoChamado.getDescricao());
+//					c.setStatus(novoChamado.getStatus());
+//					return repository.save(c);
+//				})
+//				.orElseThrow(() -> new RuntimeException("Chamado não encontrado"));
+//	}
+	
 	public Chamado atualizar(Long id, Chamado novoChamado) {
-		return repository.findById(id)
-				.map(c -> {
-					c.setTitulo(novoChamado.getTitulo());
-					c.setDescricao(novoChamado.getDescricao());
-					c.setStatus(novoChamado.getStatus());
-					return repository.save(c);
-				})
-				.orElseThrow(() -> new RuntimeException("Chamado não encontrado"));
+		var chamadoAtual = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Chamado não encontrado."));
+		
+		validarAtualizacao(chamadoAtual);
+		
+		chamadoAtual.setTitulo(novoChamado.getTitulo());
+		chamadoAtual.setDescricao(novoChamado.getDescricao());
+		chamadoAtual.setStatus(novoChamado.getStatus());
+		var chamadoAtualizado = repository.save(chamadoAtual);
+		return chamadoAtualizado;
+	}
+	
+	public void validarAtualizacao(Chamado chamado) {
+		if(chamado.getStatus() == Status.CONCLUIDO) {
+			throw new BusinessException("Chamado concluído não pode ser alterado.");
+		}
 	}
 	
 	public void deletar(Long id) {
